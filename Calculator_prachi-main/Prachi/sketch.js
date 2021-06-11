8016e28d75ab1bb585e9c8e180f4eac24bb8a356
prachi1505 <85737452+prachi1505@users.noreply.github.com>
2021-06-11 16:01:43 +0530

    Add files via upload

---
+++ Calculator_prachi-main/Prachi/sketch.js
@@ -0,0 +1,37 @@
+var num1 , num2;
+var add,mul,sub,div;
+
+function setup() {
+  createCanvas(400, 400);
+
+  num1=createInput();
+  num1.position(5,60);
+  num2=createInput();
+  num2.position(5,200);
+
+  add=createButton("ADDITON")
+  add.position(300,60)
+  add.mousePressed(addition)
+
+  sub=createButton("SUBRACTION")
+  sub.position(300,100)
+  sub.mousePressed(subraction)
+}
+
+function draw() {
+  background("skyblue");
+
+  n1=parseInt(num1.value () );
+  n2=parseInt(num2.value () );
+}
+
+function addition(){
+  console.log(n1+n2) ;
+  text(n1+n2 , 200,200);
+}
+
+function subraction (){
+  console.log(n1-n2) ;
+  text(n1+n2 , 200,200);
+}
+
